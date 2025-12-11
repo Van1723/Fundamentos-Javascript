64b934169389f5b2be5fb74fd4cd7a0998978e4a
Van1723 <vanessa.tecma@gmail.com>
2025-12-11 13:28:34 -0300

    practice_02

---
+++ condicionais.js
@@ -0,0 +1,34 @@
+function result_okay_failed(){
+   let note_one = document.getElementById('notes_one').value;
+   let note_two = document.getElementById('notes_two').value;
+   let note_tree = document.getElementById('notes_tree').value;
+   let note_four = document.getElementById('notes_four').value;
+
+
+   let media = (parseFloat(note_one) + parseFloat(note_two) + parseFloat(note_tree) +parseFloat(note_four)) / 4 ;
+   let med = parseFloat(media);
+    
+
+    if (media.valueOf >= 5) {
+        document.getElementById('result').innerHTML = 'Sua Media  foi  : ' +  ' ' + med.toFixed(2) + ' ' +'Você foi Aprovado';
+        
+    } else {
+        document.getElementById('result').innerHTML = 'Sua Media foi   : ' +  ' ' + med.toFixed(2) + ' ' + 'Você foi Reprovado';
+    }
+
+   
+
+
+
+
+}
+
+
+
+function validator(){
+    let age = document.getElementById('age').value;
+    if(age.valueOf >= 18){
+        document.getElementById('age_validator').innerHTML = 'Sua idade ' 
+    }
+
+}
